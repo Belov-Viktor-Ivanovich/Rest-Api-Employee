da51a2a9a8f04a9b1c458484689ab5fca8eea231
Виктор <103961005+Belov-Viktor-Ivanovich@users.noreply.github.com>
2024-04-05 23:16:41 +0300

    Update RestApiApplication.java

--- rest-Api/src/main/java/com/springproject/restApi/RestApiApplication.java
+++ rest-Api/src/main/java/com/springproject/restApi/RestApiApplication.java
@@ -9,12 +9,6 @@ public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
-	/*{
-        "id": 1,
-        "name": "Masha",
-        "surname": "Polandww",
-        "email": "wwww@mail.ru",
-        "dob": "2000.01.01"
-    }*/
+
 
 }
